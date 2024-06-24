9711396132e49da7f1896218725bc6bf595a1061
ahegde3 <ahegde3@outlook.com>
2024-06-23 20:07:39 -0400

    basic structure created

---
+++ src/page/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SocialIcons from "../component/SocialIcon";
+
+export default function Home() {
+  return (
+    <div>
+      <img></img>
+      <h3>Anish Hegde</h3>
+      <p>Frontend Developer</p>
+      <div>
+        <SocialIcons />
+      </div>
+      <div>
+        <a href="">Project</a>
+        <a href="">About</a>
+      </div>
+    </div>
+  );
+}
