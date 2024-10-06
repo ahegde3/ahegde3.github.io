5b4e419ccb7d3f7c83d077e4db3b2245f6396200
ahegde3 <ahegde3@outlook.com>
2024-10-05 22:54:17 -0400

    Styling

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import SocialIcons from "../component/SocialIcon";
+import profilepic from "../assets/profile-pic.jpg";
 
 export default function Home() {
   const linkStyle = { color: "inherit", textDecoration: "none" };
   return (
     <div>
       <div style={{ marginTop: "60px" }}>
-        <img src="https://avatars.githubusercontent.com/u/20241972?s=400&u=a6cfbaebeeb8d1469c1998d93434adea679324dc&v=4" />
+        <img src={profilepic} alt="profile pic" width="400" height="500" />
       </div>
       <h1 style={{ color: "black" }}>Anish Hegde</h1>
       <p>Exploring one day at a time.</p>
@@ -25,17 +26,19 @@ export default function Home() {
         <a
           href="https://drive.google.com/file/d/1X3ZVfaGYdmj9eKNU4JdS5k8sQUb9Pe2m/view?usp=sharing"
           style={linkStyle}
-          // target="_blank"
+          target="_blank"
+          rel="noreferrer"
         >
           Resume
         </a>
-        <a href="" style={linkStyle}>
+        {/* <a href="" style={linkStyle}>
           Project
-        </a>
+        </a> */}
         <a
           href="https://docs.google.com/spreadsheets/d/19eSMeadP2v6TbMUOlcDaztG-tpHgOFkAjYAc9kilikY/edit?usp=sharing"
           style={linkStyle}
-          // target="_blank"
+          target="_blank"
+          rel="noreferrer"
         >
           Reading-List
         </a>
